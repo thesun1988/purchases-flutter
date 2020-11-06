45fa0afdfdd2e05fde7251d41e802cd53ad425c9
Cesar de la Vega <cesarvegaro@gmail.com>
2020-11-05 18:23:04 -0800

    fixes activities

--- example/android/app/src/androidTest/java/com/revenuecat/purchases_flutter_example/EmbeddingV1ActivityTest.java
+++ example/android/app/src/androidTest/java/com/revenuecat/purchases_flutter_example/EmbeddingV1ActivityTest.java
@@ -1,14 +1,13 @@
-package com.revenuecat.purchases_flutter;
+package com.revenuecat.purchases_flutter_example;
 
 import androidx.test.rule.ActivityTestRule;
 
-import com.revenuecat.purchases_flutter_example.EmbeddingV1Activity;
-
-import dev.flutter.plugins.e2e.FlutterRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
-@RunWith(FlutterRunner.class)
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+
+@RunWith(FlutterTestRunner.class)
 public class EmbeddingV1ActivityTest {
     @Rule
     public ActivityTestRule<EmbeddingV1Activity> rule =
